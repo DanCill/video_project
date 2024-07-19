5681b4f64159fcedb8349e5a30a7a1ce0178d0b3
Dan Cilliers <dan.cilliers@hailer.com>
2024-07-19 13:15:08 +0300

    Basic sign up and sign in operations done using appwrite

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { Text, Image, View, ScrollView } from "react-native";
+import { Text, Image, View, ScrollView, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
 
 import { images } from "../../constants";
 import FormField from "../components/FormField";
 import CustomButton from "../components/CustomButton";
+import { signIn } from "../../lib/appwrite";
 
 const SignIn = () => {
   const [form, setForm] = useState({
@@ -15,7 +16,24 @@ const SignIn = () => {
 
   const [isSubmitting, setisSubmitting] = useState(false);
 
-  const submit = () => {};
+  const submit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert("Error", "Please fill all fields");
+    }
+
+    setisSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password);
+
+      //set it to global state later
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setisSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
